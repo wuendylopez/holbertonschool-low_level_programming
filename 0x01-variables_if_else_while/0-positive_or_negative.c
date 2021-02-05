e211f939584736201810c400de9014147cb68837
wuendylopez <wjisetdlopez@hotmail.com>
2021-02-05 04:20:07 +0000

    0x01: fix 0-pos comment

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * print sign
+ *
+ */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n < 0)
 		printf("%d is negative\n", n);
 	else if (n == 0)
