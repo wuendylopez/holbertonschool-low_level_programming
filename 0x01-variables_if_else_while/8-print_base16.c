972e974fac8d167be7650a18c8b00118054bd9e5
wuendylopez <wjisetdlopez@hotmail.com>
2021-02-06 00:52:59 +0000

    All the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hex[] = "0123456789abcdef";
+	int i;
+
+	for (i = 0; i < 16; i++)
+		putchar(hex[i]);
+	putchar('\n');
+	return (0);
+}
+
