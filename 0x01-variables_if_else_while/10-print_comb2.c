9de2027f3591dbffb73747fa45f42fd397d0644a
wuendylopez <wjisetdlopez@hotmail.com>
2021-02-06 00:58:03 +0000

    The numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numberRight;
+	int numberLeft;
+
+	for (numberLeft = 48; numberLeft <= 57; numberLeft++)
+	{
+		for (numberRight = 48; numberRight <= 57; numberRight++)
+		{
+			putchar(numberLeft);
+			putchar(numberRight);
+			if (!(numberRight ==  57 && numberLeft == 57))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
