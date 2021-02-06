7038fc3c65541ab616bc659896b8d954735ae443
wuendylopez <wjisetdlopez@hotmail.com>
2021-02-06 00:36:26 +0000

    The alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int lowerCase = 97;
+	int lastCase = 26;
+
+	for (i = 0; i < lastCase; i++)
+	{
+		putchar(lowerCase + i);
+	}
+	putchar('\n');
+	return (0);
+}
+
