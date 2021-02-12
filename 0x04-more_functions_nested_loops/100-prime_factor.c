6ff943a95c8037ec327cfa1dd0d32c6a19c31b59
wuendylopez <wjisetdlopez@hotmail.com>
2021-02-12 03:30:57 +0000

    Fnds and prints the largest prime factor of the number 612852475143

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long i, numbre = 612852475143;
+
+	for (i = 2; i < sqrt(numbre); i++)
+		while ((numbre % i) == 0)
+			numbre /= i;
+	printf("%ld\n", numbre);
+	return (0);
+}
