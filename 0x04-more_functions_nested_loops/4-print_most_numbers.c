f4ab35a1795625a71f47b42433541d59fe1b8a7c
wuendylopez <wjisetdlopez@hotmail.com>
2021-02-12 01:34:15 +0000

    prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9,
+ *                 followed by a new line.
+ * Return: 1 (is uppercase) else 1
+ */
+void print_most_numbers(void)
+{
+	char number = '/';
+
+	while ((number++) < '9')
+		if (number != '2' && number != '4')
+			_putchar(number);
+	_putchar('\n');
+
+}
