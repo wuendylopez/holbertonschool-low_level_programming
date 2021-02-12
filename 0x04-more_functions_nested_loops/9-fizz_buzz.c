8030b856d1b478f6510f40ca1b2cbbe4b55f17de
wuendylopez <wjisetdlopez@hotmail.com>
2021-02-12 03:23:52 +0000

    multiples of three print Fizz instead of the number and for the multiples of five print Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+#define Fizz "Fizz"
+#define Buzz "Buzz"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	short numbre = 1;
+
+	printf("%d", numbre);
+	while (numbre++ < 100)
+	{
+		if ((numbre % 3) == 0 && (numbre % 5) == 0)
+			printf(" %s%s", Fizz, Buzz);
+		else if ((numbre % 5) == 0)
+			printf(" %s", Buzz);
+		else if ((numbre % 3) == 0)
+			printf(" %s", Fizz);
+		else if ((numbre % 3) && (numbre % 5))
+			printf(" %d", numbre);
+	}
+	printf("\n");
+	return (0);
+}
