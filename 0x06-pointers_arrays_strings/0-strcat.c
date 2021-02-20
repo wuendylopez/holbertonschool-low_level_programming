33c610b9311b2204408fc9d6b88a47ff3be8adc0
wuendylopez <wjisetdlopez@hotmail.com>
2021-02-20 01:19:56 +0000

    Function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: concatenate string
+ * @src: source to join with dest
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest; i++)
+	{
+	}
+	for (j = 0; src; j++)
+	{
+		dest[j + i] = src[j];
+	}
+
+	return (dest);
+}
